618a5d1f44a7a477d31e2c3135f35e9cb86d72c7
mediterraneanapps <58656212+mediterraneanapps@users.noreply.github.com>
2023-04-17 11:20:21 +0100

    Add files via upload

---
+++ all_v16_enterprise/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'All V16 Enterprise modules',
+    'category': 'All',
+    'version': '1.0',
+    'description':
+        """
+            Without subscription, this module contains all the modules of the enterprise version 16
+        """,
+    "author": "Mediterranean Apps",
+    "price": "1099",
+    "currency": "EUR",
+    "support": "mediterranean.apps@gmail.com",
+    'depends': [],
+    'auto_install': True,
+    'data': [
+       
+    ],
+   "images": [
+        'static/imgs/Odoo.jpg',
+      
+    ],
+    'qweb': [
+      
+    ],
+     "license": "OPL-1",
+}
